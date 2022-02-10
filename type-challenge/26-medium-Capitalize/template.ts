86cd7003ad3c173d002fcb6a2f3e0280108648cb
chenyixiang <chenyixiang_cyx@youzan.com>
2022-02-10 11:32:13 +0800

    feat: 26 complete

--- type-challenge/26-medium-Capitalize/template.ts
+++ type-challenge/26-medium-Capitalize/template.ts
@@ -1,1 +1,36 @@
-type Capitalize<S extends string> = any
+type CapMap = {
+  a: "A";
+  b: "B";
+  c: "C";
+  d: "D";
+  e: "E";
+  f: "F";
+  g: "G";
+  h: "H";
+  i: "I";
+  j: "J";
+  k: "K";
+  l: "L";
+  m: "M";
+  n: "N";
+  o: "O";
+  p: "P";
+  q: "Q";
+  r: "R";
+  s: "S";
+  t: "T";
+  u: "U";
+  v: "V";
+  w: "W";
+  x: "X";
+  y: "Y";
+  z: "Z";
+};
+
+type Capitalize1<S extends string> = S extends ""
+  ? ""
+  : S extends `${infer P}${infer T}`
+  ? P extends keyof CapMap
+    ? `${CapMap[P]}${T}`
+    : S
+  : never;
